a2058e9df36196b9b6bab77263a42a218d6c192c
CSharpFri <CSharpFri@CLASS-2-06>
2017-12-01 19:07:44 +0100

    aj cykly

---
+++ DnzVTydnu/Cykly.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnyVTydnu
+{
+    class Cykly
+    {
+        public static void CyklusWhile()
+        {
+            int i = 0;
+
+            while (i < 4)
+            {
+
+                i++;
+                Console.WriteLine("Ahoj");
+            }
+        }
+
+        public static void DoWhile()
+        {
+            int i = 0;
+            do
+            {
+                i++;
+                Console.WriteLine("Ahoj");
+            }
+            while (i < 4);
+        }
+            
+            
+            
+        
+    }
+}
--- DnzVTydnu/DnyVTydnu.cs
+++ DnzVTydnu/DnyVTydnu.cs
@@ -13,79 +13,82 @@ namespace DnyVTydnu
             int denTydne = int.Parse(cisloDne);
 
             if (denTydne > 0 && denTydne < 8)
-            { 
+            {
                 // toto je spravny vstup
 
                 switch (denTydne)
-            {
-                case 0:
-                case 7:
-                     Console.WriteLine("Dnesni den je Nedele");
-                     break;
-                case 1:
-                    Console.WriteLine("Dnesni den je Pondeli");
-                    break;
-                case 2:
-                    Console.WriteLine("Dnesni den je Utery");
-                    break;
-                case 3:
-                    Console.WriteLine("Dnesni den je Streda");
-                    break;
-                case 4:
-                    Console.WriteLine("Dnesni den je Ctvrtek");
-                    break;
-                case 5:
-                    Console.WriteLine("Dnesni den je Patek");
-                    break;
-                case 6:
-                    Console.WriteLine("Dnesni den je Sobota");
-                    break;
-                default:
-                    Console.WriteLine("Dnesni den je 30.02., nebo si se spletl?");
-                    break;
-            }
-
-                else
                 {
-                    //chybny vstup
-                    Console.WriteLine{"Spatny vstup"};
+                    case 0:
+                    case 7:
+                        Console.WriteLine("Dnesni den je Nedele");
+                        break;
+                    case 1:
+                        Console.WriteLine("Dnesni den je Pondeli");
+                        break;
+                    case 2:
+                        Console.WriteLine("Dnesni den je Utery");
+                        break;
+                    case 3:
+                        Console.WriteLine("Dnesni den je Streda");
+                        break;
+                    case 4:
+                        Console.WriteLine("Dnesni den je Ctvrtek");
+                        break;
+                    case 5:
+                        Console.WriteLine("Dnesni den je Patek");
+                        break;
+                    case 6:
+                        Console.WriteLine("Dnesni den je Sobota");
+                        break;
+                    default:
+                        Console.WriteLine("Dnesni den je 30.02., nebo si se spletl?");
+                        break;
                 }
-
             }
+
+            else
+            {
+                //chybny vstup
+                Console.WriteLine( "Spatny vstup");
+            }
+
         }
+
         public static void VypisDenTydne(string cisloDne, bool nedeleJePrvni)
         {
             int denTydne = int.Parse(cisloDne);
 
-            if(ZkontrolujVstup (denTydne))
-            { 
+            if (ZkontrolujVstup(denTydne))
+            {
                 if (nedeleJePrvni)
-                 {
-                        DenTydne--;
-                 }
-                 VypisDenTydne(denTydne.ToString());
+                {
+                    denTydne--;
+                }
+                VypisDenTydne(denTydne.ToString());
             }
             else
             {
-                Console.WriteLine ("Spatny vstup");
+                Console.WriteLine("Spatny vstup");
             }
         }
-    }
-
-    public static bool ZkontrolujVstup(int cisloDne)
-    {
-        if (cisloDne >=1 && cisloDne <= 7)
+        public static bool ZkontrolujVstup(int cisloDne)
         {
-            Console.WriteLine("spravny vstup");
-            return true;
-        }
-        else
-        {
-            Console.WriteLine("nespravny vstup, napis od 1 do 7");
-            return false;
-        }
+            if (cisloDne >= 1 && cisloDne <= 7)
+            {
+                Console.WriteLine("spravny vstup");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("nespravny vstup, napis od 1 do 7");
+                return false;
+            }
 
-        // najkratsi zapis return(cisloDne >=1 && cisloDne <= 7);
+            // najkratsi zapis return(cisloDne >=1 && cisloDne <= 7);
 
+        }
     }
 }
+
+
+
