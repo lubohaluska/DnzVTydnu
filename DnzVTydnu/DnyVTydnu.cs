c94ea60c1890dfc6e678ec5f526fa4ba31d5f409
CSharpFri <CSharpFri@CLASS-2-06>
2017-12-01 18:05:48 +0100

    DnyVTydnu

---
+++ DnzVTydnu/DnyVTydnu.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnzVTydnu
+{
+    class DnyVTydnu
+    {
+        public static void VypisDenTydne(string cisloDne)
+        {
+            int denTydne = int.Parse(cisloDne);
+
+            if (denTydne > 0 && denTydne < 8)
+            { 
+                // toto je spravny vstup
+
+                switch (denTydne)
+            {
+                case 0:
+                case 7:
+                     Console.WriteLine("Dnesni den je Nedele");
+                     break;
+                case 1:
+                    Console.WriteLine("Dnesni den je Pondeli");
+                    break;
+                case 2:
+                    Console.WriteLine("Dnesni den je Utery");
+                    break;
+                case 3:
+                    Console.WriteLine("Dnesni den je Streda");
+                    break;
+                case 4:
+                    Console.WriteLine("Dnesni den je Ctvrtek");
+                    break;
+                case 5:
+                    Console.WriteLine("Dnesni den je Patek");
+                    break;
+                case 6:
+                    Console.WriteLine("Dnesni den je Sobota");
+                    break;
+                default:
+                    Console.WriteLine("Dnesni den je 30.02., nebo si se spletl?");
+                    break;
+            }
+
+                else
+                {
+                    //chybny vstup
+                    Console.WriteLine{"Spatny vstup"};
+                }
+
+            }
+        }
+        public static void VypisDenTydne(string cisloDne, bool nedeleJePrvni)
+        {
+            int denTydne = int.Parse(cisloDne);
+            if(denTydne > 0 && denTydne < 8)
+        { 
+                 if (nedeleJePrvni)
+                 {
+                        DenTydne--;
+                 }
+                 VypisDenTydne(denTydne.ToString());
+        }
+            else
+            {
+                Console.WriteLine
+            }
+        }
+    }
+
+    public static bool ZkontrolujVstup(int cisloDne)
+    {
+
+    }
+
+    }
+}
--- DnzVTydnu/Program.cs
+++ DnzVTydnu/Program.cs
@@ -10,6 +10,7 @@ namespace DnyVTydnu
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
